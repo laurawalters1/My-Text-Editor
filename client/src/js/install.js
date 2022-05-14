a9b3b539dded154d2fae3b993db69fd4094b9fb5
Laura Walters <laura.walters1098@yahoo.com>
2022-05-14 18:41:48 +0100

    adding webpack manifest config

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -12,6 +12,7 @@ window.addEventListener("beforeinstallprompt", (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener("click", async () => {
+  console.log("click");
   const promptEvent = window.deferredPrompt;
 
   if (!promptEvent) {
