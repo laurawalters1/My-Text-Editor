857fa31989da3df42239ea1df4ad5dcbe6b5e580
Laura Walters <laura.walters1098@yahoo.com>
2022-05-15 15:14:20 +0100

    ammending config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,7 +17,6 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "Contact Cards",
       }),
 
       // Injects our custom service worker
